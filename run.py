3f7950a2425274a976535b85cbb749aae275a5b9
HarHar <harhar-captain@live.com>
2015-09-29 15:21:26 -0300

    Split run.py into webstuff.py + ircstuff.py
    Putting the whole web thing inside a thread; this commit is needed so we can load up both the IRC and web parts in a clean manner

--- run.py
+++ run.py
@@ -1,69 +1,14 @@
 #!/usr/bin/env python2
 # -*- encoding: utf8 -*-
 import sys
+import threading
+import webstuff
 sys.dont_write_bytecode = True
 
-import eventlet
-import os
-import sys
-from flask import Flask, render_template, request, redirect, session, Response
-import socketio
-import threading
-import time
-import json
-import modules
+# start web stuff
+webThread = threading.Thread(target=webstuff.start())
+webThread.setDaemon(True)
+webThread.start()
 
-fapp = Flask(__name__)
-sio = socketio.Server()
-
-class utilities(object):
-	def __init__(self):
-		self.sid = None
-		self.sio = None
-		self.modulePath = ''
-	def staticFilePath(self, f):
-		if f[-3:] in ['css', '.js']:
-			return '/getstatic/' + os.path.join(self.modulePath, f)
-	def readFile(self, path):
-		f = open(os.path.join(self.modulePath, path), 'r')
-		data = f.read()
-		f.close()
-		return data
-	def loadJS(self, sio, filename):
-		sio.emit('loadJS', self.staticFilePath(filename))
-	def loadCSS(self, sio, filename):
-		sio.emit('loadCSS', self.staticFilePath(filename))
-	def loadHTML(self, sio, filename):
-		sio.emit('setContent', self.readFile(filename))
-globalUtils = utilities()
-
-@fapp.route('/')
-def getLoader():
-	return render_template('loader.html')
-
-@fapp.route('/main')
-def getMain():
-	return render_template('main.html')
-
-@fapp.route('/getstatic/<path:p>')
-def getStatic(p):
-	if ('..' in p) or (p.find('static') != -1) or (p.startswith('modules/') == False) or (p.find('/web/') == -1):
-		return
-	mimetype = 'text/css' if p.endswith('.css') else 'application/javascript'
-	return Response(globalUtils.readFile(p), mimetype=mimetype)
-
-@sio.on('getContent')
-def getContent(sid, which):
-	for module in modules.webmodules:
-		if which in module.web.provides:
-			utils = utilities()
-			utils.sid = sid
-			utils.modulePath = module.__name__.replace('.', '/') + '/web/'
-			module.web.provides[which](utils, sio)
-
-fapp.debug = True
-fapp.config['SECRET_KEY'] = 'hunter2'
-if __name__ == '__main__':
-    app = socketio.Middleware(sio, fapp)
-    eventlet.wsgi.server(eventlet.listen(('', 8080)), app)
-
+# start irc stuff
+pass
