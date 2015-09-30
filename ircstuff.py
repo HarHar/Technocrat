4604ae2390a186437c400f334f7e6f75e2fc1e2e
HarHar <harhar-captain@live.com>
2015-09-30 09:51:02 -0300

    Add howto use nicklist of the irc library

--- ircstuff.py
+++ ircstuff.py
@@ -3,13 +3,16 @@
 import sys
 sys.dont_write_bytecode = True
 
+import logging
+logging.basicConfig()
+
 from girc import Client
 # db still not made, need to 
 # load these things up from the bd
 # when it's done
-nick = 'Technocrat'
+nick = 'TechBot'
 channels = ['#Technocrat']
-client = Client('irc.broke-it.com', nick=nick)
+client = Client('localhost', nick=nick)
 for channel in channels: client.channel(channel).join()
 
 import modules
@@ -19,10 +22,18 @@ import modules
 #  msg.target = who received (chan/user) "#/g/bots"
 #  msg.host = sender's host "my.vhost"
 #  msg.user = username part of host "~harhar" from "~harhar@my.vhost"
+#
+# client._channels = list of channels
+# client._channels['#/g/bots'].users['@'] = list of operators on #/g/bots (includes @ and up)
+# client._channels['#/g/bots'].only('@') = list of operators on #/g/bots (ONLY @)
 
 @client.handler(command='PRIVMSG')
 def handle_privmsg(client, msg):
 	pass
+	#debugging:
+	#print repr(msg)
+	#if msg.sender == 'ne':
+	#	client.msg(msg.target, repr(eval(msg.params[1])))
 
 client.start()
 client.join()
