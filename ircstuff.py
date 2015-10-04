e76139ab5bb01ab8cd624850a3294e29a3fa689e
HarHar <harhar-captain@live.com>
2015-10-04 15:49:06 -0300

    [irc part] tried documentating girc
    BUUUUT realized this library sucks, I'm gonna replace it with https://pypi.python.org/pypi/irc probably

--- ircstuff.py
+++ ircstuff.py
@@ -12,29 +12,96 @@ from girc import Client
 # when it's done
 nick = 'TechBot'
 channels = ['#Technocrat']
-client = Client('localhost', nick=nick)
+client = Client('irc.broke-it.com', nick=nick)
 for channel in channels: client.channel(channel).join()
 
 import storage
 import modules
 
 # good things to remember:
-#  msg.sender = who sent "harhar"
-#  msg.target = who received (chan/user) "#/g/bots"
-#  msg.host = sender's host "my.vhost"
-#  msg.user = username part of host "~harhar" from "~harhar@my.vhost"
-#
+# client.msg(target, msg)
 # client._channels = list of channels
 # client._channels['#/g/bots'].users['@'] = list of operators on #/g/bots (includes @ and up)
 # client._channels['#/g/bots'].only('@') = list of operators on #/g/bots (ONLY @)
 
 @client.handler(command='PRIVMSG')
-def handle_privmsg(client, msg):
+def handle_privmsg(client, payload):
+	"""
+		Handles messages
+
+		payload properties:	
+			sender = who sent "harhar"
+			target = who received (chan/user) "#/g/bots"
+			host = sender's host "my.vhost"
+			user = username part of host "~harhar" from "~harhar@my.vhost"
+			params[1] = message
+	"""
 	pass
-	#debugging:
-	#print repr(msg)
-	#if msg.sender == 'ne':
-	#	client.msg(msg.target, repr(eval(msg.params[1])))
+
+@client.handler(command='KICK')
+def handle_kick(client, payload):
+	"""
+		Handles kicks
+			- payload.params == ['#channel', 'kickednick', 'reason']
+
+	"""
+	pass
+
+@client.handler(command='INVITE')
+def handle_invite(client, payload):
+	print 'invite ' + repr(payload.params)
+
+@client.handler(command='JOIN')
+def handle_join(client, payload):
+	""" Handles joins:
+
+		payload properties:
+			- nick = 'nick'
+			- host = 'host.name'
+			- user = '~user'
+			- params[0] = '#channel'
+	"""
+	pass
+
+@client.handler(command='PART')
+def handle_part(client, payload):
+	print 'part ' + repr(payload.params)
+	print payload.nick
+
+@client.handler(command='QUIT')
+def handle_quit(client, payload):
+	""" Handles quit messages from other users
+
+		payload properties:
+			- message: quit message
+	"""
+	print 'quit ' + repr(payload.params)
+
+@client.handler(command='NICK')
+def handle_nick(client, payload):
+	""" Handles own nick change
+
+		payload properties:
+			- nickname: new nick
+	"""
+	print 'nick ' + repr(payload.params)
+
+@client.handler(command='MODE')
+def handle_mode(client, payload):
+	""" Handles mode changes
+
+		Can be mode change of our own nick (2 params)
+		Can be mode change of a channel (3+ params)
+
+		Example:
+			payload.params == ['OurBot', '+ix']
+			payload.params == ['#channel', '+o', 'nickname']
+	"""
+	pass
+
+@client.handler(command='WHOIS')
+def handle_whois(client, payload):
+	print 'whois ' + repr(payload.params)
 
 client.start()
 client.join()
