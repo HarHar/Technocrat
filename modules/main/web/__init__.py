09c35d87ac25bd8a8b4ebb0f9637aed029ece822
HarHar <harhar-captain@live.com>
2015-10-06 07:54:28 -0300

    Cool CSS header animations
    oh so cool

--- modules/main/web/__init__.py
+++ modules/main/web/__init__.py
@@ -1,6 +1,6 @@
 def setMain(utils, client):
 	utils.loadCSS(client, 'page.css')
 	utils.loadJS(client, 'page.js')
-	utils.loadHTML(client, 'page.html')
+	utils.loadHTML(client, 'page.html', {'$title': '#/g/technology'})
 
 provides = {'main': setMain}
