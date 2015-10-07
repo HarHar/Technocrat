8e460674f313de9baa69d09033e2726dc5312862
HarHar <harhar-captain@live.com>
2015-10-07 10:58:13 -0300

    [web] cool animation is back as a loading indicator!

--- modules/main/web/page.js
+++ modules/main/web/page.js
@@ -1,7 +1,27 @@
+var ayy = true;
+
+function toggleHeaderEffect() {
+	if (ayy) {
+		$('#effect').removeClass('reverseCoolAnimation');
+		$('#effect').addClass('coolAnimation');
+		ayy = false;
+	} else {
+		$('#effect').addClass('reverseCoolAnimation');
+		$('#effect').removeClass('coolAnimation');
+		ayy = true;
+	}
+}
+
 $('.menuItem').click(function() {
 	$this = $(this);
 	$('.menuItem.selected').removeClass('selected');
 	$this.addClass('selected');
 
+	toggleHeaderEffect();
 	socket.emit('callModule', $this.attr('module'), $this.attr('method'));
+});
+
+socket.on('setRealContent', function(html) {
+	$('#effect').removeClass('coolAnimation');
+	$('#realContent').html(html);
 })
