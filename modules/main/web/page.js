c98911358996619d069857bf07e5f162e9e27351
HarHar <harhar-captain@live.com>
2015-10-12 08:56:58 -0300

    Hide mascot

--- modules/main/web/page.js
+++ modules/main/web/page.js
@@ -31,6 +31,8 @@ function toggleHeaderEffect() {
 	}
 }
 
+window.toggleHeaderEffect = toggleHeaderEffect;
+
 var spinner = '<div class="spinner"> <div class="rect1"></div> <div class="rect2"></div> <div class="rect3"></div> <div class="rect4"></div> <div class="rect5"></div> </div>'
 
 $('.menuItem').click(function() {
@@ -42,6 +44,8 @@ $('.menuItem').click(function() {
 	$('#realContent').html(spinner);
 	socket.emit('callModule', $this.attr('module'), $this.attr('method'));
 });
+
+
 
 var changingMascot;
 
